08e3109cb4fbe2719c8e8a29e5460f2155c2095c
204014 Tomaszczyk Adam <204014@edu.p.lodz.pl>
2017-10-11 22:10:42 +0200

    Dodano proste poruszanie sie po gridzie

---
+++ Assets/Scripts/FighterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterController : MonoBehaviour {
+
+	public float speed;
+
+	private bool isMoving;
+	private Vector3 movePosition;
+
+	// Use this for initialization
+	void Start () {
+		isMoving = false;
+		movePosition = new Vector3();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetMouseButtonDown(1)) {
+			isMoving = true;
+
+			Vector3 mousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			// rounding because we want to end in grid
+			movePosition.x = Mathf.Round(mousePosition3D.x);
+			movePosition.y = Mathf.Round(mousePosition3D.y);
+		}
+
+		if (isMoving && movePosition.Equals(transform.position)) {
+			isMoving = false;
+		}
+	}
+
+	void FixedUpdate () {
+		if (isMoving) {
+			transform.position = Vector2.MoveTowards(transform.position, movePosition, speed * Time.deltaTime);
+		}
+	}
+}
