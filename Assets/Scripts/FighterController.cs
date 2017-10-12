b7e1ec43d7d5da2e542a23c3a7c4ba9c604aab4a
204014 Tomaszczyk Adam <204014@edu.p.lodz.pl>
2017-10-12 13:19:35 +0200

    Dodano podstawowe zaznaczanie (single click) i poprawiono poruszanie (jednostki nie beda na siebie wchodzic)

--- Assets/Scripts/FighterController.cs
+++ Assets/Scripts/FighterController.cs
@@ -5,29 +5,31 @@ using UnityEngine;
 public class FighterController : MonoBehaviour {
 
 	public float speed;
+	public bool isSelected;
 
 	private bool isMoving;
 	private Vector3 movePosition;
+	private GameObject gameController;
 
 	// Use this for initialization
 	void Start () {
 		isMoving = false;
+		isSelected = false;
 		movePosition = new Vector3();
+		gameController = GameObject.FindGameObjectWithTag("GameController");
 	}
 	
 	// Update is called once per frame
 	void Update() {
-		if (Input.GetMouseButtonDown(1)) {
-			isMoving = true;
-
-			Vector3 mousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-			// rounding because we want to end in grid
-			movePosition.x = Mathf.Round(mousePosition3D.x);
-			movePosition.y = Mathf.Round(mousePosition3D.y);
+		// if this unit is Selected
+		if (isSelected) {
+			// then we are calculating movement
+			CalculateMovement();
 		}
 
+		// if we are moving and our position is the same as our destination
 		if (isMoving && movePosition.Equals(transform.position)) {
+			// that means we are not moving anymore
 			isMoving = false;
 		}
 	}
@@ -36,5 +38,34 @@ public class FighterController : MonoBehaviour {
 		if (isMoving) {
 			transform.position = Vector2.MoveTowards(transform.position, movePosition, speed * Time.deltaTime);
 		}
+	}
+
+	void CalculateMovement() {
+		// if right mouse button down
+		if (Input.GetMouseButtonDown(1)) {
+			// then we are moving
+			isMoving = true;
+
+			// calculationg mouse position in world
+			Vector3 mousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			// rounding because we want to end in grid
+			movePosition.x = Mathf.Round(mousePosition3D.x);
+			movePosition.y = Mathf.Round(mousePosition3D.y);
+
+			// find spot as close to our click as possible but without any units there
+			movePosition = FindFreeSpot(movePosition);
+		}
+	}
+
+	Vector3 FindFreeSpot(Vector3 mousePos) {
+		Vector3 freeSpot = mousePos;
+		gameController.GetComponent<GameController>().allUnits.ForEach(unit => {
+			if (unit.transform.position.Equals(freeSpot) && unit.Equals(gameObject) == false) {
+				freeSpot.x++;
+				freeSpot = FindFreeSpot(freeSpot);
+			}
+		});
+		return freeSpot;
 	}
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public List<GameObject> allUnits;
+
+	// Use this for initialization
+	void Start () {
+		allUnits = new List<GameObject>();
+		// not sure if GameController Start method will always run AFTER creation of all GameObjects
+		allUnits.AddRange(GameObject.FindGameObjectsWithTag("Unit"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			allUnits.ForEach(unit => {
+				Vector3 mousePosition3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+				if (Mathf.Abs(mousePosition3D.x - unit.transform.position.x) < 0.5f && 
+					Mathf.Abs(mousePosition3D.y - unit.transform.position.y) < 0.5f) {
+					unit.GetComponent<FighterController>().isSelected = true;
+					unit.GetComponent<SpriteRenderer>().color = Color.green;
+				} else {
+					unit.GetComponent<FighterController>().isSelected = false;
+					unit.GetComponent<SpriteRenderer>().color = Color.white;
+				}
+			});
+		}
+	}
+}
