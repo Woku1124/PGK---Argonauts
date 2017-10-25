9bdcd0561944d21937c3e46450e93ce0d6f461d8
Krystian Potentas <203972@edu.p.lodz.pl>
2017-10-25 09:28:50 +0200

    rozwiazano konflikt

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AsteroidController : MonoBehaviour {
-<<<<<<< HEAD
+
 	public float speed;
 	public Rigidbody2D r;
 
-=======
->>>>>>> 5e27f67fff9dff26ef9cdd5e313b28da25433a4c
+
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +16,7 @@ public class AsteroidController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
+
 		
 		//transform.Rotate(Vector3.forward * 20f * Time.deltaTime, Space.Self);
 
@@ -25,9 +24,7 @@ public class AsteroidController : MonoBehaviour {
 	void FixedUpdate () {
 
 		transform.Translate (transform.up * speed * Time.deltaTime);
-		}
-=======
+
         transform.Rotate(Vector3.forward * 5f * Time.deltaTime, Space.Self);
     }
->>>>>>> 5e27f67fff9dff26ef9cdd5e313b28da25433a4c
 }
